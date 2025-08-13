5eb7add96030557fefb811cb941d804aa0bd999d
Jules Baralotto <jules.baralotto@kedgebs.com>
2025-08-13 15:27:34 +0200

    first app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Dishoom", address: "Bassin à flots", category: 'french', phone_number: '06 45 63 82 15')
+Restaurant.create!(name: "Pizza East", address: "Pessac", category: 'italian', phone_number: '06 85 13 82 15')
+Restaurant.create!(name: "Le délire", address: "Délirium", category: 'french', phone_number: '06 25 63 32 15')
+Restaurant.create!(name: "Pie chart", address: "La victoire", category: 'chinese', phone_number: '06 95 63 63 15')
+Restaurant.create!(name: "Vin de fou", address: "St catherine", category: 'belgian', phone_number: '06 45 29 82 10')
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
